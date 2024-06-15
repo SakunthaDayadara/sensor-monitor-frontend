f6d3676ee9f137dd7928a11e247922c63224026b
Sakuntha <sakunthadayadara2001@gmail.com>
2024-06-15 17:03:53 +0530

    Authentication and protected Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
+import Login from "./Login/Login";
+import RequireAuth from "./Util/RequireAuth";
+import Register from "./Register/Register";
+import Dashboard from "./Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />}></Route>
+                <Route path='/register' element={<Register />}></Route>
+                <Route element={<RequireAuth />}>
+                    <Route path='/dashboard' element={<Dashboard />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
